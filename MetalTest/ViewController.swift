4da0789723fe19818c49cf44752279c7c828e5f1
Alvaro Santiesteban <alvaro.santiesteban@netapp.com>
2019-08-01 15:38:16 -0600

    Initial Commit

---
+++ MetalTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MetalTest
+//
+//  Created by Alvaro Santiesteban on 8/1/19.
+//  Copyright © 2019 Alvaro Santiesteban. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
